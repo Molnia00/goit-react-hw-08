d397400ca7535e69051cb6080f352d3bf2d1c90c
Molnia00 <bilankati@gmail.com>
2025-04-22 19:20:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,23 @@ import Contactss from './pages/contactss/Contactss'
 
 function App() {
 
+
+
   return (
       <Routes>
         <Route path='/' element={<Layout/>}>
            <Route index element={<Home/> }></Route>
-           
-      </Route>
-      <Route path='contacts' element={<Contactss/>}></Route>
+           <Route path='contacts' element={<Contactss/>}></Route>
       <Route path='login' element={<Login />}></Route>
       <Route path='register' element={<Register/> }></Route>
       <Route path='*' element={<NotFound />}></Route>
+      </Route>
+      
       </Routes>)
 }
 
 
+
 export default App
+
+
